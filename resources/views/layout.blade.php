e509c07fc2399f933e4e8c30bc467050d53ef2a2
Mariano Gomez <marianogomez@gmail.com>
2022-03-17 17:26:26 -0300

    Changing jQuery from minimal version to full version (with ajax features)

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -31,12 +31,11 @@
         </div>
     </div>
 @endif
-
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
 <div class="container">
     @yield('content')
 </div>
 
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
 
 </body>
